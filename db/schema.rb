45a3f2f35cae0c949a269640ebfe75bc0ab1d435
otaki444 <xm.ms.430x@gmail.com>
2020-05-10 13:33:27 +0000

    [Update]aboutページ作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_10_033024) do
+ActiveRecord::Schema.define(version: 2020_05_10_101847) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -42,6 +42,13 @@ ActiveRecord::Schema.define(version: 2020_05_10_033024) do
   create_table "images", force: :cascade do |t|
     t.integer "post_id"
     t.string "image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
