40a8a0476ad12a1fcb374ef8b628c786f3e4fa57
otaki444 <xm.ms.430x@gmail.com>
2020-05-13 11:57:36 +0000

    [Update]コメント機能,ajax

--- config/routes.rb
+++ config/routes.rb
@@ -31,7 +31,7 @@ Rails.application.routes.draw do
 
   resources :posts, only: [:index, :create, :show, :edit, :update, :destroy] do
     resource :likes, only: [:create, :destroy] #いいね
-    resource :comments, only: [:create, :destroy]
+    resources :comments, only: [:create, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
