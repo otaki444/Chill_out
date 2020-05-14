220b37539d87791c0faabddb3e54d315411c44b3
otaki444 <xm.ms.430x@gmail.com>
2020-05-14 06:02:28 +0000

    [Fix]会員詳細のページャ機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 	before_action :authenticate_user!
   def show
   	@user = User.find(params[:id])
+    @user_posts = @user.posts.page(params[:page]).reverse_order
   end
 
   def edit
