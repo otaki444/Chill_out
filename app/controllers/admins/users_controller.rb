1bf867bbc3992bc838dffeea882d376290093e60
otaki444 <xm.ms.430x@gmail.com>
2020-05-19 07:05:20 +0000

    [Update]admin_users index,show,top

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -1,11 +1,19 @@
 class Admins::UsersController < ApplicationController
 	before_action :authenticate_admin!
   def top
+  	@user_count = User.where("Date(created_at) = '#{Date.today}'").count
   end
 
   def index
+    case params[:user_case]
+    when "today"
+      @users = User.where("Date(created_at) = '#{Date.today}'").page(params[:page])
+    when "all"
+      @users = User.page(params[:page])
+    end
   end
 
   def show
+  	@user = User.find(params[:id])
   end
 end
