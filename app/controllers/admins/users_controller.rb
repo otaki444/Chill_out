dedfc09e3b4f2a0ea18a3c61e22f8bf3e2d1dd5f
otaki444 <xm.ms.430x@gmail.com>
2020-05-21 05:55:52 +0000

    [Update]devise flashmessage 日本語化

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -16,4 +16,26 @@ class Admins::UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      binding.pry
+      flash[:notice] = "変更しました"
+      redirect_to admins_user_path(@user.id)
+    else
+      @user = User.find(params[:id])
+      flash.now[:alert] = '変更に失敗しました'
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+      params.require(:user).permit(:name, :name_kana, :nickname, :profile_image, :introduction ,:one_word, :email, :come_out)
+  end
 end
