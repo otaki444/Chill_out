f132a01432780e1612e10e44720c72b7ca3ef312
otaki444 <xm.ms.430x@gmail.com>
2020-05-23 04:25:59 +0000

    [Update]レイアウト編集

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -2,5 +2,5 @@ class Inquiry < ApplicationRecord
   	validates :email, presence: true, length: {maximum:255},
                     format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
   	validates :message, presence: true
-	validates :name,    length: { in: 2..20 }
+	validates :name, presence: true, length: { in: 2..20 }
 end
