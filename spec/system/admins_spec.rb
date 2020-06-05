05bcca8866427884c1637dc05ed348e90478cf87
otaki444 <xm.ms.430x@gmail.com>
2020-06-05 14:06:06 +0000

    [test]system admin,user

--- spec/system/admins_spec.rb
+++ spec/system/admins_spec.rb
@@ -1,9 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe "Admins", type: :system do
-  before do
-    driven_by(:rack_test)
-  end
+	describe "ログイン" do
+		context 'ログイン画面に遷移' do
+			let(:admin) { create(:admin)}
+			before do
+				visit new_admin_session_path
+			end
+			it 'ログイン成功' do
+		 		fill_in 'admin[email]', with: admin.email
+		 		fill_in 'admin[password]', with: admin.password
+		 		click_button 'ログイン'
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+		 		expect(current_path).to eq(admins_top_path)
+			end
+			it 'ログイン失敗' do
+				fill_in 'admin[email]', with: ""
+		 		fill_in 'admin[password]', with: ""
+		 		click_button 'ログイン'
+
+		 		expect(current_path).to eq(new_admin_session_path)
+			end
+		end
+	end
+	describe '表示テスト' do
+		let(:admin) { create(:admin)}
+		before do
+			visit new_admin_session_path
+			fill_in 'admin[email]', with: admin.email
+		 	fill_in 'admin[password]', with: admin.password
+		 	click_button 'ログイン'
+		end
+		context 'ヘッダーの表示' do
+			it '管理者メニューが表示される' do
+				expect(page).to have_content '会員一覧'
+				expect(page).to have_content 'ログアウト'
+			end
+		end
+		context '会員情報の表示' do
+			it '会員一覧画面が表示される' do
+				visit admins_top_path
+				click_link '会員一覧', match: :first
+				expect(current_path).to eq(admins_users_path)
+			end
+		end
+	end
 end
